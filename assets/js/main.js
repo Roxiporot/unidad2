b8ae40e6d330768308268829ef2c54e19654af36
Roxana Avila Caceres <roxana.avilacaceres@gmail.com>
2022-02-17 21:49:30 -0300

    con errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 console.log("hola");
-alert();
-prompt();
-confirm();
+alert('');
+prompt('');
+confirm('');
 document.write('probando probando');
+var (10+10); esto es numero
+var ('10'+'10'); esto es String
